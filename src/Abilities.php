71341680ebf57ca04612dfbd3f7123faf64e68be
Shortcutz <Asinnersdream@gmail.com>
2018-09-30 18:54:27 +0200

    update

--- src/Abilities.php
+++ src/Abilities.php
@@ -2,8 +2,21 @@
 /**
  * @Entity(repositoryClass="AbilityRepository") @Table(name="abilities")
  */
+
+use Doctrine\Common\Collections\ArrayCollection;
+use App\Waffen;
+
+/**
+ * @Entity @Table(name="abilities")
+ **/
+
 class Abilities
 {
+    public function __construct()
+    {
+        $this->waffenliste = new ArrayCollection();
+    }
+
     /**
      * @Id @Column(type="integer") @GeneratedValue
      * @var int
@@ -24,6 +37,20 @@ class Abilities
      * @var string
      */
     protected $status;
+    /**
+     * @ManyToOne(targetEntity="User", inversedBy="assignedLearned")
+     **/
+    protected $learned;
+    /**
+     * @ManyToOne(targetEntity="User", inversedBy="assignedVisible")
+     **/
+    protected $visible;
+    /**
+     * @ManyToMany(targetEntity="Waffen")
+     **/
+    protected $waffenliste;
+
+    protected $waffen = null;
 
     public function getId()
     {
@@ -58,5 +85,37 @@ class Abilities
     public function getStatus()
     {
         return $this->status;
+    }
+
+    public function setLearnedAbility(User $learn)
+    {
+        $learn->addLearnedAbility($this);
+        $this->learned = $learn;
+    }
+
+    public function setVisibleAbility(User $visibleNow)
+    {
+        $visibleNow->addVisibleAbility($this);
+        $this->visible = $visibleNow;
+    }
+
+    public function getLearned()
+    {
+        return $this->learned;
+    }
+
+    public function getVisible()
+    {
+        return $this->visible;
+    }
+
+    public function weaponAbility(Waffen $waffe)
+    {
+        $this->waffen[] = $waffe;
+    }
+
+    public function getWaffen()
+    {
+        return $this->waffen;
     }
 }
---
+++ src/User.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @Entity @Table(name="users")
+ */
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+class User
+{
+    /**
+     * @Id @GeneratedValue @Column(type="integer")
+     * @var int
+     */
+    protected $id;
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    protected $visibleAbilities;
+    protected $learnedAbilities;
+
+    public function __construct()
+    {
+        $this->visibleAbilities = new ArrayCollection();
+        $this->learnedAbilities = new ArrayCollection();
+    }
+
+    public function addVisibleAbility(Abilities $ability)
+    {
+        $this->visibleAbilities[] = $ability;
+    }
+
+    public function addLearnedAbility(Abilities $ability)
+    {
+        $this->learnedAbilities[] = $ability;
+    }
+
+}
